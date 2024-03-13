7c159578ffec35ce6c35a30be4826b9d2ca3df06
oleg ostrovski <oleg.ostrovski@gmail.com>
2024-03-13 23:30:08 +0300

    refactoring code Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,40 +1,46 @@
 import React from 'react';
 import logo from '../../images/header/logo.png'
-import cart from '../../images/header/cart.svg'
-import user from '../../images/header/user.svg'
-import settings from '../../images/header/settings.svg'
-import {NavLink} from "react-router-dom";
 import {PATH} from "../../PATH/PATH";
+import {faCartShopping, faGear, faUser} from "@fortawesome/free-solid-svg-icons";
+import {useAppSelector} from "../../app/store";
+import {IconNav} from "./IconNav";
+import {NavLink} from "react-router-dom";
+import {useTranslation} from "react-i18next";
 
 export const Header = () => {
+    const themeValue = useAppSelector(state => state.settings.theme)
+
+    const {t} = useTranslation('header');
+
     return (
         <header>
-            <nav className={"header__container"}>
-                <div className={"header__bloc-logo"}>
+            <nav className={`header__container ${themeValue && 'dark'}`}>
+                <NavLink to={PATH.HOME} className={`header__bloc-logo ${themeValue && 'dark'}`}>
                     <img width={60} height={60} src={logo} alt="logo"/>
                     <h3>React Store</h3>
-                </div>
+                </NavLink>
 
                 <ul className={"header__nav-bloc"}>
-                    <li className={"header__nav-item"}>
-                        <NavLink to={PATH.SETTINGS} className={"header__nav-link"}>
-                            <img src={cart} alt="корзина" width={30} height={30} className={"header__nav-icon"}/>
-                            <span className={"header__nav-text"}>корзина</span>
-                        </NavLink>
-                    </li>
-                    <li className={"header__nav-item"}>
-                        <NavLink to={PATH.SETTINGS} className={"header__nav-link"}>
-                            <img src={user} alt="пользователь" width={30} height={30} className={"header__nav-icon"}/>
-                            <span>войти</span>
-                        </NavLink>
-                    </li>
-                    <li className={"header__nav-item"}>
-                        <NavLink to={PATH.SETTINGS} className={"header__nav-link"}>
-                            <img src={settings} alt="пользователь" width={30} height={30}
-                                 className={"header__nav-icon"}/>
-                            <span>настройки</span>
-                        </NavLink>
-                    </li>
+                    <IconNav
+                        title={t('корзина')}
+                        path={PATH.CART}
+                        themeValue={themeValue}
+                        icon={faCartShopping}
+                    />
+
+                    <IconNav
+                        title={t('войти')}
+                        path={PATH.LOGIN}
+                        themeValue={themeValue}
+                        icon={faUser}
+                    />
+
+                    <IconNav
+                        title={t('настройки')}
+                        path={PATH.SETTINGS}
+                        themeValue={themeValue}
+                        icon={faGear}
+                    />
                 </ul>
             </nav>
         </header>
