d17475213ab1e9139cea18291d6372a22d0fcfc9
oleg ostrovski <oleg.ostrovski@gmail.com>
2024-03-19 16:31:29 +0300

    add reducers in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -4,17 +4,21 @@ import {SettingsActionType, settingsReducer} from "../components/store/settings-
 import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
 import {ThunkDispatch} from "redux-thunk";
 import {StoreActionType, storeReducer} from "../components/store/store-reducer";
+import {ProductActionType, productsReducer} from "../components/store/products-reducer";
+import {LoginActionType, loginReducer} from "../components/store/login-reducer";
 
 
 const rootReducer = combineReducers({
     settings: settingsReducer,
-    items: storeReducer
+    items: storeReducer,
+    products: productsReducer,
+    loginUser: loginReducer,
 })
 
 export type AppRootStateType = ReturnType<typeof rootReducer>;
 export const store = createStore(rootReducer);
 
-type AllAction = SettingsActionType | StoreActionType
+type AllAction = SettingsActionType | StoreActionType | ProductActionType | LoginActionType
 
 export type AppDispatchType = ThunkDispatch<AppRootStateType, any, AllAction>
 export const useAppDispatch = () => useDispatch<AppDispatchType>()
