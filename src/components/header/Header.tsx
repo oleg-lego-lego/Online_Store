85c1b85152a741f4d7e4173399e1341fe1ff5fdf
oleg ostrovski <oleg.ostrovski@gmail.com>
2024-03-19 16:37:15 +0300

    add logic in Header.tsx

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from '../../images/header/logo.png'
 import {PATH} from "../../PATH/PATH";
-import {faCartShopping, faGear, faUser} from "@fortawesome/free-solid-svg-icons";
+import {faCartShopping, faGear, faHouse, faUser} from "@fortawesome/free-solid-svg-icons";
 import {useAppSelector} from "../../app/store";
-import {IconNav} from "./IconNav";
 import {NavLink} from "react-router-dom";
 import {useTranslation} from "react-i18next";
+import {IconNavLogout} from "./IconNavLogout";
+import {IconNav} from "./IconNav";
+
 
 export const Header = () => {
     const themeValue = useAppSelector(state => state.settings.theme)
+    const logoutValue = useAppSelector(state => state.loginUser.logoutValue)
 
     const {t} = useTranslation('header');
 
@@ -16,11 +19,18 @@ export const Header = () => {
         <header>
             <nav className={`header__container ${themeValue && 'dark'}`}>
                 <NavLink to={PATH.HOME} className={`header__bloc-logo ${themeValue && 'dark'}`}>
-                    <img width={60} height={60} src={logo} alt="logo"/>
+                    <img width={70} height={70} src={logo} alt="logo"/>
                     <h3>React Store</h3>
                 </NavLink>
 
                 <ul className={"header__nav-bloc"}>
+                    <IconNav
+                        title={t('главная')}
+                        path={PATH.HOME}
+                        themeValue={themeValue}
+                        icon={faHouse}
+                    />
+
                     <IconNav
                         title={t('корзина')}
                         path={PATH.CART}
@@ -28,12 +38,14 @@ export const Header = () => {
                         icon={faCartShopping}
                     />
 
-                    <IconNav
-                        title={t('войти')}
-                        path={PATH.LOGIN}
-                        themeValue={themeValue}
-                        icon={faUser}
-                    />
+                    {!logoutValue &&
+                        <IconNav
+                            title={t('войти')}
+                            path={PATH.LOGIN}
+                            themeValue={themeValue}
+                            icon={faUser}
+                        />
+                    }
 
                     <IconNav
                         title={t('настройки')}
@@ -41,6 +53,8 @@ export const Header = () => {
                         themeValue={themeValue}
                         icon={faGear}
                     />
+
+                    {logoutValue && <IconNavLogout/>}
                 </ul>
             </nav>
         </header>
